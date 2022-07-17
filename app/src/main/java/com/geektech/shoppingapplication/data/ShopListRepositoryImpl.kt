b93ee14fb41c6a0d00a80563026e7eaa9fb4fb1d
E-Dastann <erkinovd36@gmail.com>
2022-07-17 19:09:25 +0600

    space

--- app/src/main/java/com/geektech/shoppingapplication/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/geektech/shoppingapplication/data/ShopListRepositoryImpl.kt
@@ -15,6 +15,8 @@ class ShopListRepositoryImpl : ShopListRepository {
         shopList.add(shopItem)
     }
 
+
+
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
     }
